8f898036a42cc816f0e42a51a867a41308fbeec2
SirRango <sturdivantdevon78@gmail.com>
2024-10-24 04:04:07 -0500

    Added enviroment variables and configured vite.config and removed vite_paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
+  },
+  define: {
+    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
   }
 })
