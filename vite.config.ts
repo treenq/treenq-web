f550b5acc077133753ba10fa411f25be89b0dcd4
SirRango <sturdivantdevon78@gmail.com>
2024-10-24 12:38:21 -0500

    removed the define from vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     }
   },
-  define: {
-    "process.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
-  }
 })
