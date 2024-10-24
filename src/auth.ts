cfb08bbd840fce6d84e058ecfc535505967fe5f6
SirRango <sturdivantdevon78@gmail.com>
2024-10-24 08:08:40 -0500

    removed the VITE prefix and the unused variables

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,13 @@
 import { createZitadelAuth } from "@zitadel/react";
 
 const config: ZitadelConfig = {
-    authority: import.meta.env.VITE_ZITADEL_AUTHORITY,
-    client_id: import.meta.env.VITE_ZITADEL_CLIENT_ID,
-    redirect_uri: import.meta.env.VITE_ZITADEL_REDIRECT_URI,
-    response_type: import.meta.env.VITE_ZITADEL_RESPONSE_TYPE,
-    scope: import.meta.env.VITE_ZITADEL_SCOPE,
-    post_logout_redirect_uri: import.meta.env.VITE_ZITADEL_LOGOUT_URI,
-    response_mode: import.meta.env.VITE_ZITADEL_RESPONSE_MODE,
+    authority: import.meta.env.ZITADEL_AUTHORITY,
+    client_id: import.meta.env.ZITADEL_CLIENT_ID,
+    redirect_uri: import.meta.env.ZITADEL_REDIRECT_URI,
+    response_type: import.meta.env.ZITADEL_RESPONSE_TYPE,
+    scope: import.meta.env.ZITADEL_SCOPE,
+    post_logout_redirect_uri: import.meta.env.ZITADEL_LOGOUT_URI,
+    response_mode: import.meta.env.ZITADEL_RESPONSE_MODE,
   };
 
   const auth = createZitadelAuth(config)
